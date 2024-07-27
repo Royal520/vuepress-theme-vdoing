bc77053c79945ba77010eecef2b010b9dd7839e5
lovejj <i@lovejj.tk>
2024-07-28 00:30:01 +0800

    feat: docker 使用

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -64,6 +64,7 @@ module.exports = {
       { text: 'Linux', link: '/pages/linux/0601/',items: [
           { text: 'Linux', link: '/pages/linux/0601/'},
           { text: 'keepalived', link: '/pages/linux/0602/' },
+          { text: 'docker', link: '/pages/linux/docker/' },
         ] },
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
